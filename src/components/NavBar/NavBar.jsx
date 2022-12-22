206f37d9cdbc065d03952dad82803f7f283cdbfd
Ignacio Zamora <ignaciojo.zamora@gmail.com>
2022-12-22 11:34:45 -0300

    navbar + flex wrapper

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import "./navbar.css"
+
+
+function NavBar () {
+    return(
+        <ul>
+            <li>Cerveza</li>
+            <li>Vodka</li>
+            <li>Gin</li>
+            <li>Gaseosas</li>
+        </ul>
+    )
+}
+
+export default NavBar;
---
+++ src/components/flexWrapper/FlexWrapper.jsx
@@ -0,0 +1,9 @@
+import "./flexwrapper.css";
+
+
+
+function FlexWrapper(props) {
+    <div className="flex">{props.children}</div>
+}
+
+export default FlexWrapper;
